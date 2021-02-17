0d29f37253e0a21356c2d118c2f06bbb22661def
ArturoV4 <75847917+ArturoV4@users.noreply.github.com>
2021-02-16 23:43:40 -0500

    Commit final

--- trabajadores_cansados.py
+++ trabajadores_cansados.py
@@ -1,77 +1,76 @@
 import time
 import threading
 import sys
-class Semaphore(object):
-    def __init__(self, initial):
+
+class Semaforo(object):
+    def __init__(self, inicial):
         self.lock = threading.Condition(threading.Lock())
-        self.value = initial
+        self.valor = inicial
 
     def up(self):
         with self.lock:
-            self.value += 1
+            self.valor += 1
             self.lock.notify()
 
     def down(self):
         with self.lock:
-            while self.value == 0:
+            while self.valor == 0:
                 self.lock.wait()
-            self.value -=1
+            self.valor -=1
 
-class ChopStick(object):
-    def __init__(self, number):
-        self.number = number
-        self.user = -1
+class Sillas(object):
+    def __init__(self, id):
+        self.id = id
         self.lock = threading.Condition(threading.Lock())
-        self.taken = False
+        self.tomado = False
 
-    def take(self, user):
+    def tomar(self, persona):
         with self.lock:
-            while self.taken == True:
+            while self.tomado == True:
                 self.lock.wait()
-            self.user = user
-            self.taken = True
-            sys.stdout.write("Trabajador [%s] toma la silla: %s\n" % (user, self.number))
+            self.persona = persona
+            self.tomado = True
+            sys.stdout.write("Trabajador [%s] toma la silla: %s\n" % (persona, self.id))
             self.lock.notifyAll()
 
-    def drop(self, user):
+    def dejar(self, persona):
         with self.lock:
-            while self.taken == False:
+            while self.tomado == False:
                 self.lock.wait()
-            self.user = -1
-            self.taken = False
-            sys.stdout.write("Trabajador [%s] deja la silla: %s\n" % (user, self.number))
+            self.tomado = False
+            sys.stdout.write("Trabajador [%s] deja la silla: %s\n" % (persona, self.id))
             self.lock.notifyAll()
 
-class Philosopher (threading.Thread):
-    def __init__(self, number, left, right, butler):
+class Trabajador (threading.Thread):
+    def __init__(self, id, silla, silla_ady, butler):
         threading.Thread.__init__(self)
-        self.number = number
-        self.left = left
-        self.right = right
+        self.id = id
+        self.silla = silla
+        self.silla_ady = silla_ady
         self.butler = butler
 
     def run(self):
         for i in range(1):
             self.butler.down()
-            print("Trabajador ", self.number, "de pie")
+            print("Trabajador ", self.id, "de pie")
             time.sleep(0.1)
-            self.left.take(self.number)
+            self.silla.tomar(self.id)
             time.sleep(0.1)
-            self.right.take(self.number)
-            print("Trabajador ",self.number, "descansando")
+            self.silla_ady.tomar(self.id)
+            print("Trabajador ", self.id, "descansando")
             time.sleep(0.1)
-            self.right.drop(self.number)
-            self.left.drop(self.number)
+            self.silla_ady.dejar(self.id)
+            self.silla.dejar(self.id)
             self.butler.up()
-        sys.stdout.write("Trabajador [%s] termina de descansar" % self.number)
+        sys.stdout.write("Trabajador [%s] termina de descansar" % self.id)
 
 def main():
-    n = 4
-    butler = Semaphore(n-1)
-    c = [ChopStick(i) for i in range(n)]
-    p = [Philosopher(i, c[i], c[(i+1) % n], butler) for i in range(n)]
+    n = 5
+    butler = Semaforo(n-1)
+    s = [Sillas(i) for i in range(n)]
+    t = [Trabajador(i, s[i], s[(i+1) % n], butler) for i in range(n)]
     for i in range(n):
-        p[i].start()
+        t[i].start()
 
 
 if __name__=="__main__":
